29b637b06c60d9af7f169f0bff72c9bfa1c3d9c9
sandeep-megaska <sandeep.lalu@gmail.com>
2025-12-26 23:30:05 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Bigonbuy ERP</h1>
-      <p>Initial setup</p>
+    <div style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>Bigonbuy Console</h1>
+      <p>Select a module:</p>
+
+      <ul style={{ lineHeight: 2 }}>
+        <li>
+          <Link href="/erp">📦 ERP Dashboard</Link>
+        </li>
+      </ul>
+
+      <p style={{ marginTop: 24, opacity: 0.7 }}>
+        (Internal use only)
+      </p>
     </div>
   );
 }
