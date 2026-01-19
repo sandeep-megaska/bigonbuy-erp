ee44c1fee5bc02e8240f3ed2f452da66aa3a7a57
sandeep-megaska <sandeep.lalu@gmail.com>
2026-01-19 20:10:11 +0300

    Update test.ts

--- pages/api/integrations/amazon/test.ts
+++ pages/api/integrations/amazon/test.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import crypto from "crypto";
-import fetch from "node-fetch";
+
 
 /**
  * STEP 1: Get LWA Access Token using refresh token
