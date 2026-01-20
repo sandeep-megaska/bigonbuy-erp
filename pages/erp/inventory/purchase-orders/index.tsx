5b1c633f6310e215b09f97686875bead39edae33
sandeep-megaska <sandeep.lalu@gmail.com>
2026-01-20 17:19:29 +0300

    Update index.tsx

--- pages/erp/inventory/purchase-orders/index.tsx
+++ pages/erp/inventory/purchase-orders/index.tsx
@@ -3,7 +3,6 @@ import type { FormEvent } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import ErpShell from "../../../../components/erp/ErpShell";
-
 import {
   cardStyle,
   eyebrowStyle,
