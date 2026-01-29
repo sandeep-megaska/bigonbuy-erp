d115d94ab06629a898d399a0446f9d3e8ee6352a
sandeep-megaska <sandeep.lalu@gmail.com>
2026-01-29 13:04:58 +0300

    Fix employees API import path (#458)

--- pages/api/erp/hr/employees/index.ts
+++ pages/api/erp/hr/employees/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import handleEmployees from "../../../../lib/erp/hr/employeesHandler";
+import handleEmployees from "../../../../../lib/erp/hr/employeesHandler";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   return handleEmployees(req, res);
